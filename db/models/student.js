bed28b4c49e86afb7e7a3f01503a7fa4c64b7d00
Michael Baez <mrbaez427@gmail.com>
2021-05-01 11:08:46 -0400

    Completed Models tests

--- db/models/student.js
+++ db/models/student.js
@@ -1,6 +1,24 @@
 const Sequelize = require('sequelize');
 const db = require('../db');
 
-const Student = db.define();
+const Student = db.define('student', {
+  firstName: { type: Sequelize.STRING, allowNull: false },
+  lastName: { type: Sequelize.STRING, allowNull: false },
+  email: {
+    type: Sequelize.STRING,
+    allowNull: false,
+    validate: { isEmail: true }
+  }
+});
+
+Student.addHook('beforeCreate', user => {
+  user.firstName =
+    user.firstName.charAt(0).toUpperCase() + user.firstName.slice(1);
+});
+
+Student.addHook('beforeCreate', user => {
+  user.lastName =
+    user.lastName.charAt(0).toUpperCase() + user.lastName.slice(1);
+});
 
 module.exports = Student;
