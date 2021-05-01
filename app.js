077cb4435c9b5dd4c6bb785757c0188aa4da19bc
Michael Baez <mrbaez427@gmail.com>
2021-05-01 13:33:05 -0400

    Completed tests

--- app.js
+++ app.js
@@ -21,20 +21,18 @@ app.use((err, req, res, next) => {
 });
 
 const init = async () => {
-
-  if (require.main === module){
+  if (require.main === module) {
     //will only run when run with npm start and not with npm test to avoid db syncing in multiple threads when running tests
     try {
       await db.sync();
       app.listen(3000, () => {
         console.log('Server is listening on port 3000!');
-      })
+      });
     } catch (err) {
       console.error(err);
     }
   }
-
-}
+};
 
 init();
 
